c179986b61b03e71d5d47976a33da0b28e09d4ca
Davide Guerri <davide.guerri@gmail.com>
2010-10-26 10:02:13 +0000

    Mobile_fu upgraded to support safari ipad UA. 'mobile views' disabled for touchscreen devices. Flash graph disabled for apple devices

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,12 @@ module ApplicationHelper
     bytes > 1024 ? bytes > 1048576 ? bytes > 1073741824 ? bytes > 1099511627776 ? (bytes / 1099511627776).to_s + " TBytes" : (bytes / 1073741824).to_s + " GBytes" : (bytes / 1048576).to_s + " MBytes" : (bytes / 1024).to_s + " KBytes" : (bytes).to_s + " Bytes"
   end
 
+  def is_touchscreen_device?
+    is_device?('iphone') || is_device?('ipod') || is_device?('ipad') || is_device?('android')
+  end
+
+  def is_apple_device?
+    is_device?('iphone') || is_device?('ipod') || is_device?('ipad')
+  end
+  
 end
