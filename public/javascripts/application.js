9c8697aa871d953429506bc5255a46ac187426e1
Matteo Latini <mtylty@gmail.com>
2011-04-12 12:08:17 +0000

    adds more usable js missing message (only displayed when js or ajax is not available)

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -8,6 +8,17 @@ var owums = {
         return ($(selector).length > 0);
     },
 
+    hideWhenJsIsAvailable: function(selector) {
+        if ($.support.ajax) {
+            $(document).ready(function(){
+                $(selector).hide();
+            });
+            $(document).ajaxComplete(function(){
+                $(selector).hide();
+            });
+        }
+    },
+
     //Change verification methods on signup
     toggleVerificationMethod: function() {
         $('[id$=verification_method]').change(function(){
