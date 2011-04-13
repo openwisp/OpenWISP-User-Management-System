f11348f32e694eeb1d9970363e836d919027dade
Matteo Latini <mtylty@gmail.com>
2011-04-13 12:56:45 +0000

    fixes bug #37

--- config/initializers/exception_notification.rb
+++ config/initializers/exception_notification.rb
@@ -1,6 +1,6 @@
-recipients = Configuration.get('exception_notification_recipients').split(',')
-sender = Configuration.get('exception_notification_sender')
-email_subject_prefix = Configuration.get('exception_notification_email_prefix')
+recipients = Configuration.get('exception_notification_recipients').split(',') rescue 'root@localhost'
+sender = Configuration.get('exception_notification_sender') rescue 'root@localhost'
+email_subject_prefix = Configuration.get('exception_notification_email_prefix') rescue '[OWUMS] '
 
 ExceptionNotification::Notifier.exception_recipients = recipients
 ExceptionNotification::Notifier.sender_address = sender
