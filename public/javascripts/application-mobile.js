0fccc13c76c03e2f739eb0a0af88553b41f82730
Matteo Latini <mtylty@gmail.com>
2011-04-14 12:11:44 +0000

    adds cache flush when changing locale (for mobile ui)

--- public/javascripts/application-mobile.js
+++ public/javascripts/application-mobile.js
@@ -6,3 +6,14 @@ $(document).bind('pageshow', function(){
     $('select.disabled').selectmenu('disable');
     owums.toggleVerificationMethod();
 });
+
+$(document).bind('pageshow', function(){
+    $('.locale_link').bind('click', function() {
+        $(document).one('pagehide', function(){
+            $('[data-url=""]').remove();
+            $('[data-url="/"]').remove();
+        });
+        $('.ui-page').not('.ui-page-active').remove();
+    });
+});
+
