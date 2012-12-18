10f5851b9cc03be4d5bf0bd1e9e34fc987021ed7
nemesisdesign <federico.capoano@teletu.it>
2012-12-18 14:49:54 +0100

    added a couple of unit tests for Account model

---
+++ test/unit/account_test.rb
@@ -0,0 +1,70 @@
+require 'test_helper'
+
+class AccountTest < ActiveSupport::TestCase
+  test "missing username should fail" do
+    # create a new account with missing username
+    a = Account.new
+    a.given_name = 'Fred'
+    a.surname = 'Bar'
+    a.email = 'fred.bar@testing.com'
+    a.email_confirmation = a.email
+    a.password = 'testerpwd3'
+    a.password_confirmation = a.password
+    a.mobile_prefix = ''
+    a.mobile_suffix = ''
+    a.mobile_prefix_confirmation = ''
+    a.mobile_suffix_confirmation = ''
+    a.verification_method = 'gestpay_credit_card'
+    a.birth_date = CONFIG['birth_date'] ? '1980-10-10' : ''
+    a.address = CONFIG['address'] ? 'Via dei Tizii 6' : ''
+    a.city = CONFIG['city'] ? 'Rome' : ''
+    a.zip = CONFIG['zip'] ? '00185' : ''
+    a.state = 'Italy'
+    a.eula_acceptance = true
+    a.privacy_acceptance = true
+    
+    # ensure it doesn't get saved
+    assert !a.save
+    # there should be only one error related to the missing username
+    assert a.errors.length <= 1
+    assert a.errors.has_key?(:username)
+  end
+  
+  test "check gestpay phase1 is working" do
+    # create a new account
+    a = Account.new
+    a.given_name = 'Fred'
+    a.surname = 'Bar'
+    a.username = 'tester'
+    a.email = 'fred.bar@testing.com'
+    a.email_confirmation = a.email
+    a.password = 'testerpwd3'
+    a.password_confirmation = a.password
+    a.mobile_prefix = ''
+    a.mobile_suffix = ''
+    a.mobile_prefix_confirmation = ''
+    a.mobile_suffix_confirmation = ''
+    a.verification_method = 'gestpay_credit_card'
+    a.birth_date = CONFIG['birth_date'] ? '1980-10-10' : ''
+    a.address = CONFIG['address'] ? 'Via dei Tizii 6' : ''
+    a.city = CONFIG['city'] ? 'Rome' : ''
+    a.zip = CONFIG['zip'] ? '00185' : ''
+    a.state = 'Italy'
+    a.eula_acceptance = true
+    a.privacy_acceptance = true
+    
+    # save into DB
+    assert a.save
+    # ensure retrieve_gestpay_url doesn't return nil
+    assert !a.retrieve_gestpay_url.nil?
+    
+    # retrieve parameter B from gestpay_url
+    querystring = a.retrieve_gestpay_url.split('?')[1]
+    params = querystring.split('&')
+    puts params
+    b = params[1].gsub('b=', '')
+    
+    # ensure param b is not empty
+    assert !b.empty?
+  end
+end
