df60725caddd66c97d3f115dbd1060cafdd6cb9e
nemesisdesign <federico.capoano@teletu.it>
2013-07-08 17:58:19 +0200

    radius accountings API draft

--- app/controllers/radius_accountings_controller.rb
+++ app/controllers/radius_accountings_controller.rb
@@ -17,8 +17,8 @@
 
 class RadiusAccountingsController < ApplicationController
   before_filter :require_operator
-  before_filter :load_user
-  before_filter :load_radius_accountings
+  #before_filter :load_user
+  #before_filter :load_radius_accountings
 
   skip_before_filter :set_mobile_format
 
@@ -32,29 +32,63 @@ class RadiusAccountingsController < ApplicationController
 
   respond_to :xml, :json
 
+  # TODO:
+  # last vs limit?
+  # limit uses less resources
+  # @user.radius_accountings.with_full_name.limit(params[:last].to_i).order("id DESC").reverse
+  # use .scoped to clean up and DRY
+  # generalize last and day
+  # implement 404 (look for user first, then select radius accountings which have that username)
+
+  # GET /radius_accountings?[limit=N|ap=MAC_ADDRESS]
   # GET /users/:user_id/radius_accountings?[day=YYYY-MM-DD|last=N]
   def index
+    # if requesting accountings of a user
+    unless params[:user_id].nil?
+      @user = User.find_by_id_or_username!(params[:user_id])
+      # filter by date if 
+      if params[:day] and Date.parse(params[:day])
+        @radius_accountings = @user.radius_accountings.with_full_name.on_day(Date.parse(params[:day]))
+      elsif params[:last]
+        @radius_accountings = @user.radius_accountings.with_full_name.last(params[:last].to_i)
+      else
+        @radius_accountings = @user.radius_accountings.with_full_name
+      end
+    # requesting all the accountings
+  else
+      # determine limit if present, otherwise default limit is 50, maximum limit is also 50
+      limit = (!params[:limit].nil? and params[:limit].to_i <= 50) ? params[:limit] : 50
+      
+      # filter AP if requested
+      unless params[:ap].nil?
+        # convert semicolon-separted mac address in uppercase dash-separated
+        # eg: 00:15:6D:9F:CF:EC becomes 00-15-6D-9F-CF-EC
+        mac = params[:ap].gsub(':', '-').upcase
+        @radius_accountings = RadiusAccounting.with_full_name.where("CalledStationId LIKE ?", "#{mac}:%").order("AcctStartTime DESC").limit(limit)
+      else
+        @radius_accountings = RadiusAccounting.with_full_name.order("AcctStartTime DESC").limit(limit)
+      end
+    end
 
     respond_to do |format|
       format.xml
       format.json { render :json => @radius_accountings.to_json }
     end
-
   end
 
   private
 
-  def load_user
-    @user = User.find_by_id_or_username!(params[:user_id])
-  end
-
-  def load_radius_accountings
-    if params[:day] and Date.parse(params[:day])
-      @radius_accountings = @user.radius_accountings.on_day(Date.parse(params[:day]))
-    elsif params[:last]
-      @radius_accountings = @user.radius_accountings.last(params[:last].to_i)
-    else
-      @radius_accountings = @user.radius_accountings
-    end
-  end
+  #def load_user
+  #  @user = User.find_by_id_or_username!(params[:user_id])
+  #end
+  #
+  #def load_radius_accountings
+  #  if params[:day] and Date.parse(params[:day])
+  #    @radius_accountings = @user.radius_accountings.on_day(Date.parse(params[:day]))
+  #  elsif params[:last]
+  #    @radius_accountings = @user.radius_accountings.last(params[:last].to_i)
+  #  else
+  #    @radius_accountings = @user.radius_accountings
+  #  end
+  #end
 end
--- app/views/radius_accountings/index.xml.builder
+++ app/views/radius_accountings/index.xml.builder
@@ -1,10 +1,15 @@
 xml.radius_accountings do
   @radius_accountings.each do |radius_accounting|
     xml.radius_accounting do
+      xml.username radius_accounting.username
+      xml.full_name "%s %s" % [radius_accounting.given_name, radius_accounting.surname]
+      xml.framed_ip_address radius_accounting.framed_ip_address
+      xml.calling_station_id radius_accounting.calling_station_id
       xml.acct_start_time radius_accounting.acct_start_time
       xml.acct_stop_time radius_accounting.acct_stop_time == '0000-00-00 00:00:00' ? nil : radius_accounting.acct_stop_time
       xml.realm radius_accounting.realm
       xml.nas_ip_address radius_accounting.nas_ip_address
+      xml.called_station_id radius_accounting.called_station_id
       xml.acct_session_id radius_accounting.acct_session_id
       xml.acct_unique_id radius_accounting.acct_unique_id
       xml.acct_input_octets radius_accounting.acct_input_octets
