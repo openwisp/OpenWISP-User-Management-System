ee7e3882b20f6f6778b01e49409dc44aea45141d
Andrea Ferraresi <a.ferraresi@caspur.it>
2013-07-22 10:41:44 +0200

    fixed some stuff that causes unworking migration script

--- db/migrate/20130314133354_add_roles_to_admin.rb
+++ db/migrate/20130314133354_add_roles_to_admin.rb
@@ -1,6 +1,9 @@
 class AddRolesToAdmin < ActiveRecord::Migration
   def self.up
     admin = Operator.find_by_login("admin")
+    if admin.nil?
+      return
+    end
     unless admin.roles.include?("registrant_by_nothing")
       admin.roles = admin.roles + ["registrant_by_nothing"]
     end
@@ -11,6 +14,9 @@ class AddRolesToAdmin < ActiveRecord::Migration
 
   def self.down
     admin = Operator.find_by_login("admin")
+    if admin.nil?
+      return
+    end
     if admin.roles.include?("registrant_by_nothing")
       admin.roles = admin.roles.delete_if { |i| i == "registrant_by_nothing" }
     end
