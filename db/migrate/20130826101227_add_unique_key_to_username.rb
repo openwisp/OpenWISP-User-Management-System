dcfb58f8d0cca54a8f78e8b7e2d125a22cb8464b
nemesisdesign <federico.capoano@teletu.it>
2014-03-12 10:39:27 +0100

    AddUniqueKeyToUsername migration remove_index fix insert instruction in a begin/rescue block to avoid aborting the migrations in case the index is not found.

--- db/migrate/20130826101227_add_unique_key_to_username.rb
+++ db/migrate/20130826101227_add_unique_key_to_username.rb
@@ -1,9 +1,14 @@
 class AddUniqueKeyToUsername < ActiveRecord::Migration
   def self.up
-    remove_index :users, :name => :index_users_on_username
+    # ignore errors in case index was not there (it might happen)
+    begin
+      remove_index :users, :name => :index_users_on_username
+    rescue
+      nil
+    end
     add_index :users, :username, :unique => true
   end
-  
+
   def self.down
     remove_index :users, :name => :index_users_on_username
     add_index :users, :username
