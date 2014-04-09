d3687978c7bb5f84955f9e00ca428c8b8f997726
nemesisdesign <federico.capoano@teletu.it>
2014-04-09 16:41:11 +0200

    Added support to Furlan language (disabled by default)

---
+++ db/migrate/20140409142151_add_fur_language_config_keys.rb
@@ -0,0 +1,34 @@
+class AddFurLanguageConfigKeys < ActiveRecord::Migration
+  keys = YAML::load(File.open("db/fixtures/configurations.yml"))
+  
+  @configurations = [
+    keys[120],
+    keys[121],
+    keys[122],
+    keys[123],
+    keys[124],
+    keys[125],
+    keys[126],
+    keys[127],
+    keys[128],
+    keys[129],
+    keys[130],
+  ]
+  
+  def self.up
+    @configurations.each do |config|
+      if Configuration.find_by_key(config['key']).nil?
+        Configuration.set(config['key'], config['value'])
+      end
+    end
+  end
+
+  def self.down
+    @configurations.each do |config|
+      c = Configuration.find_by_key(config['key'])
+      unless c.nil?
+        c.destroy
+      end
+    end
+  end
+end
