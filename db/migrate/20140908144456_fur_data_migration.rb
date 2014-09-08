9645891ec804a43540edf6c8d72dbd4d0fa9bdd1
nemesisdesign <federico.capoano@teletu.it>
2014-09-08 17:04:44 +0200

    Revised Friulian linguistic minority

---
+++ db/migrate/20140908144456_fur_data_migration.rb
@@ -0,0 +1,26 @@
+class FurDataMigration < ActiveRecord::Migration
+  keys = YAML::load(File.open("db/fixtures/configurations.yml"))
+
+  @configurations = [
+    keys[120],
+    keys[121],
+    keys[122],
+    keys[123],
+    keys[124],
+    keys[125],
+    keys[126],
+    keys[127],
+    keys[128],
+    keys[129],
+    keys[130],
+  ]
+
+  def self.up
+    @configurations.each do |config|
+      Configuration.set(config['key'], config['value'])
+    end
+  end
+
+  def self.down
+  end
+end
