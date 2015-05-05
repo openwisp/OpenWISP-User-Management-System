d6ca9d7a3c5b36d708d329c7642d023fe2aa28fd
nemesisdesign <nemesis@ninux.org>
2015-05-05 18:34:14 +0200

    Added authorization model for social login #52

---
+++ app/models/authorization.rb
@@ -0,0 +1,6 @@
+class Authorization < ActiveRecord::Base
+  belongs_to :account
+  belongs_to :user
+  validates_presence_of :user_id, :uid, :provider
+  validates_uniqueness_of :uid, :scope => :provider
+end
