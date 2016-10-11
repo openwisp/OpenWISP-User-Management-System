458b60a493e09febf844fc0f3d3d6f6b84a6d2dd
Ilaria De Marinis <ilaria.demarinis@openwisp.org>
2016-10-11 16:09:19 +0200

    Added skip_errors, skip_options during response validation

--- app/controllers/saml_controller.rb
+++ app/controllers/saml_controller.rb
@@ -1,36 +1,36 @@
 class SamlController < ApplicationController
   def init
     requestsaml = OneLogin::RubySaml::Authrequest.new
-    appo=request.inspect
     if request.fullpath.ends_with? 'spid'
        redirect_to(requestsaml.create(saml_settings('spid')))
     else
        redirect_to root_url, :flash => {
-         :error => "#{I18n.t(:Saml_login_failed)}: #{appo}"
+         :error => "#{I18n.t(:Spid_login_failed)}: #{appo}"
        }
        return nil
     end
   end
 
   def consume
-    response          = OneLogin::RubySaml::Response.new(params[:SAMLResponse])
+    options = CONFIG['spid_options'].first
+    response = OneLogin::RubySaml::Response.new(params[:SAMLResponse], options)
     if request.fullpath.ends_with? 'spid'  
 	response.settings = saml_settings('spid')
     else 
        redirect_to root_url, :flash => {
-         :error => "#{I18n.t(:Saml_login_failed)}: #{request.env['REQUEST_PATH']}"
+         :error => "#{I18n.t(:Spid_login_failed)} Fullpath #{response.inspect}" 
        }
        return nil
     end
     # We validate the SAML Response and check if the user already exists in the system
-    if response.is_valid?
+    if response.is_valid? || response.errors.include?(CONFIG['spid_skip_errors'])
        # authorize_success, log the user
        session[:userid] = response.nameid
        session[:attributes] = response.attributes
        @account = Account.find_or_create_from_saml(session[:attributes], CONFIG['spid_attributes'].first)
-       unless @account.errors.empty?
+       unless @account.errors.empty? 
           redirect_to root_url, :flash => {
-              :error => @account.errors
+              :error => "#{I18n.t(:Spid_login_failed)} Account errors #{response.inspect}"
           }
           return nil
        end
@@ -40,42 +40,30 @@ class SamlController < ApplicationController
           @account.current_login_ip = request.remote_ip
           @account.save!
           @account.captive_portal_login!
-          flash[:notice] = "#{I18n.t(:Login_successful)}: #{@account.username} #{session.inspect}"
+          flash[:notice] = "#{I18n.t(:Login_successful)}: #{@account.username}"
           # determine URL for redirect (defaults to account URL)
           config_url = Configuration.get('social_login_success_url', '')
           redirect_url = config_url != '' ? config_url : account_url
           redirect_to redirect_url
-          #redirect_to account_path, :flash => {
-          #   :notice => "#{I18n.t(:Login_successful)}: #{session.inspect}"
-          #}
 
     end
     else
        redirect_to root_url, :flash => {
-         :error => "#{I18n.t(:Saml_login_failed)}: #{response.inspect}"
+         :error => "#{I18n.t(:Spid_login_failed)} #{response.inspect}"
        }
     end
   end
 
   def metadata
-    #settings = Account.get_saml_settings
     meta = OneLogin::RubySaml::Metadata.new
     if request.fullpath.ends_with? 'spid'
        render :xml => meta.generate(saml_settings('spid')), :content_type => "application/samlmetadata+xml"
     else
     redirect_to root_url, :flash => {
-      :error => "#{I18n.t(:Saml_login_failed)}: #{response.inspect}"
+      :error => "#{I18n.t(:Spid_login_failed)}: #{response.inspect}"
     }
     end
   end
- 
-  protected
-  def failure
-    redirect_to root_url, :flash => {
-      :error => "#{I18n.t(:Saml_login_failed)}: #{response}"
-    }
-  end
-
 
   private
 
@@ -86,11 +74,10 @@ class SamlController < ApplicationController
     settings.assertion_consumer_service_url = "#{request.protocol}#{request.host}:#{request.port}/consume/"+service
     settings.issuer                         = "#{request.protocol}#{request.host}:#{request.port}/metadata/"+service
     settings.idp_sso_target_url             = CONFIG[service]["idp_sso_target_url"]
-    #settings.idp_cert_fingerprint           = OneLoginAppCertFingerPrint
     settings.name_identifier_format         = CONFIG[service]["name_identifier_format"]
     xml = IO.read(CONFIG[service]["metadata_file"])
     hash = Hash.from_xml(xml)
-    settings.idp_cert = hash["EntityDescriptor"]["IDPSSODescriptor"]["KeyDescriptor"][1]["KeyInfo"]["X509Data"]["X509Certificate"]
+    settings.idp_cert = hash["EntityDescriptor"]["Signature"]["KeyInfo"]["X509Data"]["X509Certificate"]
     settings.certificate = IO.read(CONFIG[service]["certificate_file"])
     settings.private_key = IO.read(CONFIG[service]["private_key_file"]) 
 
@@ -101,9 +88,12 @@ class SamlController < ApplicationController
     settings.attributes_index = 5
     # Optional. Describe an attribute consuming service for support of additional attributes.
     settings.attribute_consuming_service.configure do
-      service_name "Service"
+      service_name "Registrazione Utenti WiFi con SPID"
       service_index 5
-      add_attribute :name => "Name", :name_format => "Name Format", :friendly_name => "Friendly Name"
+      add_attribute :name => "email", :name_format => "urn:oasis:names:tc:SAML:1.1:nameid-format:unspecified", :friendly_name => "Indirizzo e-mail"
+      add_attribute :name => "Name", :name_format => "urn:oasis:names:tc:SAML:1.1:nameid-format:unspecified", :friendly_name => "Nome"
+      add_attribute :name => "givenName", :name_format => "urn:oasis:names:tc:SAML:1.1:nameid-format:unspecified", :friendly_name => "Cognome"
+      add_attribute :name => "fiscalNumber", :name_format => "urn:oasis:names:tc:SAML:1.1:nameid-format:unspecified", :friendly_name => "Codice fiscale"
     end
 
     settings
