32641174d6041564bb83351767bcf6cd8a6f1c59
Ilaria De Marinis <ilaria.demarinis@openwisp.org>
2017-03-31 14:27:53 +0200

    Multiple owmw site to convert radius accountings in "MAC-ADDRESS" FORMAT

--- app/models/associated_user.rb
+++ app/models/associated_user.rb
@@ -1,18 +1,32 @@
 class AssociatedUser < ActiveResource::Base
-  self.site = OWMW["url"]
-  self.user = OWMW["username"]
-  self.password = OWMW["password"]
+  #self.site = OWMW["url"]
+  #self.user = OWMW["username"]
+  #self.password = OWMW["password"]
   
+  attr_accessor :site, :user, :password
   # return access point mac address from user mac address
   # return false if OWMW is not configured
   def self.access_point_mac_address_by_user_mac_address(mac)
     # ensure OWMW is enabled
     if OWMW != {}
       # if owmw is enabled ensure is configured correctly
-      if OWMW["password"].nil? or OWMW["url"].nil? or OWMW["url"].empty?
+     OWMW.size.times{ |owmw|
+      if OWMW[owmw]["password"].nil? or OWMW[owmw]["url"].nil? or OWMW[owmw]["url"].empty?
         raise "OWMW not configured correctly, check configuration for environment '#{Rails.env}'"
       end
-      AssociatedUser.find(mac).access_point.mac_address
+
+      begin 
+         AssociatedUser.site = OWMW[owmw]["url"]
+         AssociatedUser.user = OWMW[owmw]["username"]
+         AssociatedUser.password = OWMW[owmw]["password"]
+         if AssociatedUser.find(mac).access_point.mac_address
+            return AssociatedUser.find(mac).access_point.mac_address
+         end
+      rescue Exception => e
+	puts "Error in executing access_point_mac_address_by_user_mac_address "+e.to_s
+	next
+      end
+     }
     else
       return false
     end
